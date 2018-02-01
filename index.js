3affb34169a115ce9e7b2050e866bd402c49f42e
jonahheeren <jonah.heeren@me.com>
2018-02-01 16:16:19 -0500

    Added example config

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const PORT = process.env.PORT || 8080;
 
-var express = require('express');
+var express = require('express'),
+    nconf   = require('nconf');
 
 var app = express();
 
+nconf.file({
+  file: './config/config.json'
+});
+if (!Object.keys(nconf.get()).length) {
+  throw new Error('Unable to load config file. Check to make sure config/config.json exists');
+}
+
 app.get('/', function(req, res) { 
-    res.sendStatus(200);
+  res.sendStatus(200);
 });
 
 var server = app.listen(PORT, function() {
-    console.log('Running Server on Port: ' + PORT);
+  console.log('Running Server on Port: ' + PORT);
 });
