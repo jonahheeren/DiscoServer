0e53fc6c8dc86bfdbb73b6c7cb05cb4e84efec30
jonahheeren <jonah.heeren@me.com>
2018-02-23 08:24:28 -0500

    Change function name

--- routes/exchangesRoutes.js
+++ routes/exchangesRoutes.js
@@ -3,7 +3,7 @@ var db = require('../database/db');
 var exchangeRouter = express.Router();
 
 exchangeRouter.get('/:exchangeName/allPairs', function(req,res){
-	db.getAllPairsByExchange(req.params.exchangeName).then(function(data) {
+	db.getPairsByExchange(req.params.exchangeName).then(function(data) {
     res.send(data);
   }).catch(function(error) {
     res.sendStatus(500);
